a9e5ff0cdb80ab18eb8b20c26f809b8699ffab20
Lau <lau.priego@gmail.com>
2015-05-09 16:45:33 +0200

    IMPLEMENTADOS Y FUNCIONALES TOCHARARRAY() Y SONIGUALES()

--- TestCadenaAlumnos/test/CadenaAlumnoTest.java
+++ TestCadenaAlumnos/test/CadenaAlumnoTest.java
@@ -1,52 +1,149 @@
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 
-public class CadenaAlumnoTest {
+public class CadenaAlumnoTest 
+{
+	/*@BeforeClass 
+	public static void crearCadenas()
+	{
+		CadenaAlumno c = new CadenaAlumno();
+		String cadena1 = "entornos";
+	}*/
+	
+	
+	//VALOR STRING
+	@Test
+	public void testToCharArray1() 
+	{
+		CadenaAlumno cadena = new CadenaAlumno();
+		
+		char[] resultadoReal = cadena.toCharArray("HOLA");
+		
+		/*LO USE PARA PROBAR SI REALMENTE ME DAB AERROR CUANDO USABA ASSERTEQUALS
+		 * for(int i=0;i<resultadoReal.length;i++)
+		{
+			System.out.println(resultadoReal[i]);
+		}*/
+		
+		char[] resultadoEsperado = {'H', 'O', 'L', 'A'};
+		
+		/*
+		for(int i=0;i<resultadoReal.length;i++)
+		{
+			System.out.println(resultadoReal[i]);
+		}*/
+		
+		//al principio usando assertEquals me daba error en la prueba, lo solucione con assertArrayEquals
+		assertArrayEquals(resultadoReal, resultadoEsperado);
+	}
+	
+	//VALOR NULL
+	@Test
+	public void testToCharArray2()
+	{
+		CadenaAlumno cadena = new CadenaAlumno();
+		
+		char[] resultadoReal = cadena.toCharArray(null);
+		
+		char[] resultadoEsperado = null;
+
+		assertArrayEquals(resultadoReal, resultadoEsperado);
+	}
 
 	@Test
-	public void testToCharArray() {
+	public void testSonIguales1() 
+	{
+		CadenaAlumno cadena = new CadenaAlumno();
+		
+		String s1 = "entornos";
+		String s2 = "ento";
+		
+		boolean resultadoReal = cadena.sonIguales(s1,s2);		
+		boolean resultadoEsperado = s1.equals(s2);
+		assertEquals(resultadoReal, resultadoEsperado);
+	}
+	
+	@Test
+	public void testSonIguales2() 
+	{
+		CadenaAlumno cadena = new CadenaAlumno();
+		
+		String s1 = "ento";
+		String s2 = "entornos";
+		
+		boolean resultadoReal = cadena.sonIguales(s1,s2);		
+		boolean resultadoEsperado = s1.equals(s2);
+		assertEquals(resultadoReal, resultadoEsperado);
+	}
+	
+	@Test
+	public void testSonIguales3() 
+	{
+		CadenaAlumno cadena = new CadenaAlumno();
+		
+		String s1 = "entornos";
+		String s2 = "sonrotne";
+		
+		boolean resultadoReal = cadena.sonIguales(s1,s2);		
+		boolean resultadoEsperado = s1.equals(s2);
+		assertEquals(resultadoReal, resultadoEsperado);
+	}
+	
+	@Test
+	public void testSonIguales4() 
+	{
+		CadenaAlumno cadena = new CadenaAlumno();
+		
+		String s1 = "entornos";
+		String s2 = "entornos";
+		
+		boolean resultadoReal = cadena.sonIguales(s1,s2);		
+		boolean resultadoEsperado = s1.equals(s2);
+		assertEquals(resultadoReal, resultadoEsperado);
+	}
+
+	@Test
+	public void testEsMayor() 
+	{
 		fail("No implementado aun");
 	}
 
 	@Test
-	public void testSonIguales() {
+	public void testEsMenor() 
+	{
 		fail("No implementado aun");
 	}
 
 	@Test
-	public void testEsMayor() {
+	public void testComparaIgnorandoMayusculas() 
+	{
 		fail("No implementado aun");
 	}
 
 	@Test
-	public void testEsMenor() {
+	public void testReemplazaTodos() 
+	{
 		fail("No implementado aun");
 	}
 
 	@Test
-	public void testComparaIgnorandoMayusculas() {
+	public void testReemplazaPrimero() 
+	{
 		fail("No implementado aun");
 	}
 
 	@Test
-	public void testReemplazaTodos() {
+	public void testMuestraSplitLimite() 
+	{
 		fail("No implementado aun");
 	}
 
 	@Test
-	public void testReemplazaPrimero() {
-		fail("No implementado aun");
-	}
-
-	@Test
-	public void testMuestraSplitLimite() {
-		fail("No implementado aun");
-	}
-
-	@Test
-	public void testMuestraSplit() {
+	public void testMuestraSplit() 
+	{
 		fail("No implementado aun");
 	}
 
